33e1fc7335d6b76001ea93aced38f50aff48a48c
ERMICH Reda <ermich.reda@ofppt-edu.ma>
2022-09-02 22:06:06 +0100

    add check data first

--- src/views/countries/Countries.vue
+++ src/views/countries/Countries.vue
@@ -1,82 +1,126 @@
 <template>
-  <!--   filter & search Bar-->
-  <div class="container-fluid gap-5 px-3 px-sm-4 px-md-5 pt-5">
-    <div class="row">
-      <div class="col-12 col-sm-6 col-lg-5 my-1 my-sm-1 position-relative">
-        <i id='loop' class="fa-solid fa-magnifying-glass position-absolute top-50 translate-middle-y "></i>
-        <input type="search" name="search" id="search" v-model="search"
-               class="w-100 form-control shadow-sm py-3 pe-2"
-               placeholder="Search for a country...">
-      </div>
-      <div class="col-12 col-sm-2 col-md-3 col-lg-4 my-1 my-sm-1"></div>
-      <div class="col-6  col-sm-4 col-md-3 col-lg-3 my-4 my-sm-1">
-        <select name="filter" id="filter" class="form-select p-3 shadow-sm" v-model="selected">
-          <option value="" hidden selected> Filter by Region</option>
-          <option value="All" >All Countries</option>
-          <option v-for="region of regions" :value="region" > {{ region }}</option>
-        </select>
-      </div>
-    </div>
-  </div>
+	<!--   filter & search Bar-->
+	<div class="container-fluid gap-5 px-3 px-sm-4 px-md-5 pt-5">
+		<div class="row">
+			<div class="col-12 col-sm-6 col-lg-5 my-1 my-sm-1 position-relative">
+				<i
+					id="loop"
+					class="fa-solid fa-magnifying-glass position-absolute top-50 translate-middle-y"
+				></i>
+				<input
+					type="search"
+					name="search"
+					id="search"
+					v-model="search"
+					class="w-100 form-control shadow-sm py-3 pe-2"
+					placeholder="Search for a country..."
+				/>
+			</div>
+			<div class="col-12 col-sm-2 col-md-3 col-lg-4 my-1 my-sm-1"></div>
+			<div class="col-6 col-sm-4 col-md-3 col-lg-3 my-4 my-sm-1">
+				<select
+					name="filter"
+					id="filter"
+					class="form-select p-3 shadow-sm"
+					v-model="selected"
+				>
+					<option value="" hidden selected>Filter by Region</option>
+					<option value="All">All Countries</option>
+					<option v-for="region of regions" :value="region">
+						{{ region }}
+					</option>
+				</select>
+			</div>
+		</div>
+	</div>
 
-  <!--  countries data-->
-  <div
-      class="home container-fluid d-flex flex-wrap justify-content-center justify-content-md-start justify-content-md-between gap-5 px-3 px-md-5 mt-5">
-    <div class="card shadow" v-if="countriesFiltered.length" v-for="country in countriesFiltered"
-         :key="country.name.common">
-      <img :src="country.flags.png" class="card-img-top h-50" alt="...">
-      <div class="card-body text-start p-4">
-        <router-link :to="{name:'Country',params:{id:country.name.common}}" class="nav-link text-dark">
-          <h3 class="my-3">{{ country.name.common }}</h3>
-        </router-link>
-        <p class="my-1 country-info">Population: <span>{{ country.population.toLocaleString("en") }}</span></p>
-        <p class="my-0 country-info">Region: <span>{{ country.region }}</span></p>
-        <p class="my-1 country-info">Capital: <span>{{ country.capital[0] }}</span></p>
-      </div>
-    </div>
-    <div v-else class="vh-100">
-      <p class="lead" v-if="!isDone">Countries is Loading ... </p>
-      <p class="lead" v-else>No Countries Found</p>
-    </div>
-  </div>
+	<!--  countries data-->
+	<div
+		class="home container-fluid d-flex flex-wrap justify-content-center justify-content-md-start justify-content-md-between gap-5 px-3 px-md-5 mt-5"
+	>
+		<div
+			class="card shadow"
+			v-if="countriesFiltered.length"
+			v-for="country in countriesFiltered"
+			:key="country.name.common"
+		>
+			<img :src="country.flags.png" class="card-img-top h-50" alt="..." />
+			<div class="card-body text-start p-4">
+				<router-link
+					:to="{ name: 'Country', params: { id: country.name.common } }"
+					class="nav-link text-dark"
+				>
+					<h3 class="my-3">{{ country.name.common }}</h3>
+				</router-link>
+				<p v-show="country.population" class="my-1 country-info">
+					Population: <span>{{ country.population.toLocaleString('en') }}</span>
+				</p>
+				<p v-show="country.region" class="my-0 country-info">
+					Region: <span>{{ country.region }}</span>
+				</p>
+				<p v-show="country.capital.length" class="my-1 country-info">
+					Capital: <span>{{ country.capital[0] }}</span>
+				</p>
+			</div>
+		</div>
+		<div v-else class="vh-100">
+			<p class="lead" v-if="!isDone">Countries is Loading ...</p>
+			<p class="lead" v-else>No Countries Found</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import {ref, watch, watchEffect} from "vue";
-import getCountries from "@/composables/getCountries";
+import { ref, watch, watchEffect } from 'vue';
+import getCountries from '@/composables/getCountries';
 
 export default {
-  name: 'Home',
-  setup() {
-    let search = ref('');
-    let isDone = ref(false);
-    let selected = ref('All');
-    let {countries, regions, countriesFiltered, getAllCountries, filterByRegion} = getCountries();
+	name: 'Home',
+	setup() {
+		let search = ref('');
+		let isDone = ref(false);
+		let selected = ref('All');
+		let {
+			countries,
+			regions,
+			countriesFiltered,
+			getAllCountries,
+			filterByRegion,
+		} = getCountries();
 
-    getAllCountries();
+		getAllCountries();
 
-    // Methods
-    watchEffect(() => {
-      if (search.value) {
-        countriesFiltered.value = countries.value.filter(country => country.name.common.toLowerCase().includes(search.value.toLowerCase()));
-        isDone.value = !isDone.value
-      } else {
-        countriesFiltered.value = countries.value;
-      }
-    });
+		// Methods
+		watchEffect(() => {
+			if (search.value) {
+				countriesFiltered.value = countries.value.filter(country =>
+					country.name.common.toLowerCase().includes(search.value.toLowerCase())
+				);
+				isDone.value = !isDone.value;
+			} else {
+				countriesFiltered.value = countries.value;
+			}
+		});
 
-    watch(selected,() => {
-      if (selected.value !== 'All') {
-        filterByRegion(selected.value);
-        isDone.value = !isDone.value
-      } else {
-        countriesFiltered.value = countries.value;
-      }
-    });
-    return {countries, countriesFiltered, search, regions, isDone, selected, filterByRegion}
-  },
-
-}
+		watch(selected, () => {
+			if (selected.value !== 'All') {
+				filterByRegion(selected.value);
+				isDone.value = !isDone.value;
+			} else {
+				countriesFiltered.value = countries.value;
+			}
+		});
+		return {
+			countries,
+			countriesFiltered,
+			search,
+			regions,
+			isDone,
+			selected,
+			filterByRegion,
+		};
+	},
+};
 </script>
 
 <style scoped lang="scss">
@@ -170,5 +214,4 @@ export default {
     width: 17rem;
   }
 }
-
 </style>
--- src/views/countries/DetailsOfCountry.vue
+++ src/views/countries/DetailsOfCountry.vue
@@ -1,77 +1,115 @@
 <template>
-  <div class="container py-5 px-lg-0 fs-5">
-    <router-link to="/">
-      <button class="btn btn-white px-5 ps-4 fs-6 ms-4 ms-lg-0"><i class="fa-solid fa-arrow-left-long fa-lg mx-2"></i>Back
-      </button>
-    </router-link>
-    <div v-if="country.length" class="row justify-content-center mt-5 gx-5 ">
-      <div class="col-12 col-lg-5 text-center my-5 my-lg-0 ">
-        <img :src="country[0].flags.png" alt="" class="shadow" >
-      </div>
-      <div class="col-12 col-lg-7 ">
-        <h1 class="fw-bold fs-1 mb-3 mt-2 mb-lg-3 mt-lg-5">{{ country[0].name.common }}</h1>
-        <div class="d-block d-lg-flex flex">
-          <ul class="p-0 w-50 ">
-            <li class="nav-link text-start "><span class="fw-bold me-2">Native Name: </span>{{
-                country[0].name.nativeName[Object.keys(country[0].name.nativeName)[0]].common
-              }}</li>
-            <li class="nav-link"><span class="fw-bold me-2">Population: </span>{{ country[0].population.toLocaleString("en") }}</li>
-            <li class="nav-link"><span class="fw-bold me-2">Region: </span>{{ country[0].region }}</li>
-            <li class="nav-link"><span class="fw-bold me-2">Sub Region: </span>{{ country[0].subregion }}</li>
-            <li class="nav-link"><span class="fw-bold me-2">Capital: </span>{{ country[0].capital[0] }}</li>
-          </ul>
-          <ul class="p-0 w-50 ms-lg-4">
-            <li class="nav-link"><span class="fw-bold me-2">Top Level Domain:</span>
-              <span v-for="top in country[0].tld">{{ top }}</span>
-            </li>
-            <li class="nav-link"><span class="fw-bold me-2">Currencies: </span>{{ Object.keys(country[0].currencies)[0] }}</li>
-            <li class="nav-link"><span class="fw-bold me-2">Languages:</span>
-<!--              <span v-for="lang in Object.values(country[0].languages)">{{ lang }}</span>-->
-              {{ Object.values(country[0].languages).join(', ') }}
-            </li>
-          </ul>
-        </div>
-        <div class="d-lg-flex flex-wrap align-items-center">
-          <h5 class="mt-4 mt-lg-1 mb-3 me-3 fw-bold" >Border Countries:</h5>
-          <router-link :to="{name:'Country',params:{id:nameCountry.name.common}}"
-                       v-for="nameCountry in borders " v-if="borders.length"
-                       @click="getCountryByName(nameCountry.name.common)">
-            <button class="btn btn-sm btn-lg-lg shadow px-4 me-2 mb-2 ">
-              {{ nameCountry.name.common }}
-            </button>
-          </router-link>
-          <p v-else>No Borders Found!!</p>
-        </div>
-      </div>
-    </div>
-    <div v-else-if="errors" class="container my-5">
-      <p>404</p>
-      <p>This is not the web page you are looking for.</p>
-      <p>Click in the  button Back to get principle page.</p>
-    </div>
-    <div v-else class="container my-5">
-      <p>Data Is Loading ...</p>
-    </div>
-  </div>
+	<div class="container py-5 px-lg-0 fs-5">
+		<router-link to="/">
+			<button class="btn btn-white px-5 ps-4 fs-6 ms-4 ms-lg-0">
+				<i class="fa-solid fa-arrow-left-long fa-lg mx-2"></i>Back
+			</button>
+		</router-link>
+		<div v-if="country.length" class="row justify-content-center mt-5 gx-5">
+			<div class="col-12 col-lg-5 text-center my-5 my-lg-0">
+				<img :src="country[0].flags.png" alt="" class="shadow" />
+			</div>
+			<div class="col-12 col-lg-7">
+				<h1 class="fw-bold fs-1 mb-3 mt-2 mb-lg-3 mt-lg-5">
+					{{ country[0].name.common }}
+				</h1>
+				<div class="d-block d-lg-flex flex">
+					<ul class="p-0 w-50">
+						<li
+							v-show="Object.keys(country[0].name.nativeName).length"
+							class="nav-link text-start"
+						>
+							<span class="fw-bold me-2">Native Name: </span
+							>{{
+								country[0].name.nativeName[
+									Object.keys(country[0].name.nativeName)[0]
+								].common
+							}}
+						</li>
+						<li v-show="country[0].population" class="nav-link">
+							<span class="fw-bold me-2">Population: </span
+							>{{ country[0].population.toLocaleString('en') }}
+						</li>
+						<li v-show="country[0].region" class="nav-link">
+							<span class="fw-bold me-2">Region: </span>{{ country[0].region }}
+						</li>
+						<li v-show="country[0].subregion" class="nav-link">
+							<span class="fw-bold me-2">Sub Region: </span
+							>{{ country[0].subregion }}
+						</li>
+						<li v-show="country[0].capital.length" class="nav-link">
+							<span class="fw-bold me-2">Capital: </span
+							>{{ country[0].capital[0] }}
+						</li>
+					</ul>
+					<ul class="p-0 w-50 ms-lg-4">
+						<li v-show="country[0].tld.length" class="nav-link">
+							<span class="fw-bold me-2">Top Level Domain:</span>
+							<span v-for="top in country[0].tld">{{ top }}</span>
+						</li>
+						<li
+							v-show="Object.keys(country[0].currencies).length"
+							class="nav-link"
+						>
+							<span class="fw-bold me-2">Currencies: </span
+							>{{ Object.keys(country[0].currencies)[0] }}
+						</li>
+						<li
+							v-show="Object.values(country[0].languages).length"
+							class="nav-link"
+						>
+							<span class="fw-bold me-2">Languages:</span>
+							<!--              <span v-for="lang in Object.values(country[0].languages)">{{ lang }}</span>-->
+							{{ Object.values(country[0].languages).join(', ') }}
+						</li>
+					</ul>
+				</div>
+				<div
+					v-show="borders.length"
+					class="d-lg-flex flex-wrap align-items-center"
+				>
+					<h5 class="mt-4 mt-lg-1 mb-3 me-3 fw-bold">Border Countries:</h5>
+					<router-link
+						:to="{ name: 'Country', params: { id: nameCountry.name.common } }"
+						v-for="nameCountry in borders"
+						@click="getCountryByName(nameCountry.name.common)"
+					>
+						<button class="btn btn-sm btn-lg-lg shadow px-4 me-2 mb-2">
+							{{ nameCountry.name.common }}
+						</button>
+					</router-link>
+				</div>
+			</div>
+		</div>
+		<div v-else-if="errors" class="container my-5">
+			<p>404</p>
+			<p>This is not the web page you are looking for.</p>
+			<p>Click in the button Back to get principle page.</p>
+		</div>
+		<div v-else class="container my-5">
+			<p>Data Is Loading ...</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import getCountry from "@/composables/getCountry";
+import getCountry from '@/composables/getCountry';
 
 export default {
-  name: 'Country',
-  props: {
-    id: {
-      type: String,
-      required: true
-    }
-  },
-  setup(props) {
-    let {country, borders,errors ,getCountryById, getCountryByName} = getCountry();
-    getCountryById(props.id);
-    return {country, borders, errors, getCountryByName};
-  },
-}
+	name: 'Country',
+	props: {
+		id: {
+			type: String,
+			required: true,
+		},
+	},
+	setup(props) {
+		let { country, borders, errors, getCountryById, getCountryByName } =
+			getCountry();
+		getCountryById(props.id);
+		return { country, borders, errors, getCountryByName };
+	},
+};
 </script>
 
 <style scoped lang="scss">
@@ -107,5 +145,3 @@ button {
   background: $lightModeInput;
 }
 </style>
-
-
